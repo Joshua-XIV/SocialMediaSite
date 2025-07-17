2208baf0a16c1732c467fb206ae4872dbbb674a3
TheBulletRain <josh.hdz956@yahoo.com>
2025-07-17 00:04:48 -0500

    search page url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 import HomePage from "./pages/HomePage.tsx";
 import MainLayout from "./layouts/MainLayout.tsx";
-import PostPage from "./pages/PostPage.tsx"
+import PostPage from "./pages/PostPage.tsx";
 import CommentPage from "./pages/CommentPage.tsx";
 import JobPage from "./pages/JobPage.tsx";
 import CreateJobPage from "./pages/CreateJobPage.tsx";
+import SearchPage from "./pages/SearchPage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -13,10 +14,11 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: '/post/:id', element: <PostPage/>},
-      { path: '/comment/:id', element: <CommentPage/>},
-      { path: '/jobs', element: <JobPage/>},
-      { path: '/jobs/create', element: <CreateJobPage/>}
+      { path: "/post/:id", element: <PostPage /> },
+      { path: "/comment/:id", element: <CommentPage /> },
+      { path: "/jobs", element: <JobPage /> },
+      { path: "/jobs/create", element: <CreateJobPage /> },
+      { path: "/search", element: <SearchPage /> },
     ],
   },
 ]);
@@ -25,9 +27,13 @@ const App = () => {
   return (
     <>
       <RouterProvider router={router} />
-      <Toaster position="top-right" richColors toastOptions={{style: {marginTop : '2rem'}}}/>
+      <Toaster
+        position="top-right"
+        richColors
+        toastOptions={{ style: { marginTop: "2rem" } }}
+      />
     </>
-  )
+  );
 };
 
 export default App;
